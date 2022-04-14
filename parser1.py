2d10cd90d6e641e3255294c6e48ec5f017cc3263
MaksimBondarev <66637696+PhillKroger@users.noreply.github.com>
2022-04-14 09:56:53 +0300

    Update parser1.py

--- parser1.py
+++ parser1.py
@@ -1,9 +1,14 @@
 import requests
+
 from bs4 import BeautifulSoup
 from deep_translator import GoogleTranslator as GT
 
+
 url = 'https://scrapingclub.com/exercise/list_basic/'
-params = {'page': 1}
+params = {
+    'page': 1,
+    }
+
 pages = 2
 n = 1
 x=1
@@ -11,12 +16,11 @@ x=1
 def f():
     def translate(text, target='ru', source='auto'):
         return GT(source=source, target=target).translate(text)
-    global d
-    global x
+    global d, x
+    
     for n, i in enumerate(items, start=1):
         item_name = i.find('h4', class_='card-title').text.strip()
         item_price = i.find('h5').text
-
         url = d[n-1]
         response = requests.get('https://scrapingclub.com' + url)
         soup = BeautifulSoup(response.text, 'lxml')
@@ -24,7 +28,6 @@ def f():
         text = translate(text)
         print(f'{x}: Название: {item_name}\n Цена: {item_price}\n Описание: {text}')
         x+=1
-
 
 while params['page'] <= pages:
     response = requests.get(url, params=params)
